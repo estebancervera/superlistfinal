f9bacb852cc2ab9af790c11091328d8476cf793b
Danielr19 <51176392+Danielr19@users.noreply.github.com>
2021-11-20 23:49:26 -0600

    Master List Activity (#3)
    * First Commit
    
    * Added Main Activity Menu Buttons, Fragments, and added back button from newlistactivity
    
    * Master List Activity
    
    Co-authored-by: Esteban Cervera <cerverae18@hotmail.com>

--- app/src/main/java/com/cerverae18/superlistfinal/Category.kt
+++ app/src/main/java/com/cerverae18/superlistfinal/Category.kt
@@ -1,5 +1,6 @@
 package com.cerverae18.superlistfinal
 
+
 import java.io.Serializable
 
 data class Category(val name: String): Serializable
---
+++ app/src/main/java/com/cerverae18/superlistfinal/MasterListActivity.kt
@@ -0,0 +1,93 @@
+package com.cerverae18.superlistfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.cerverae18.superlistfinal.databinding.ActivityMasterListBinding
+import android.content.DialogInterface
+
+import android.text.Editable
+import android.text.Layout
+import android.widget.LinearLayout
+
+
+
+
+
+
+
+class MasterListActivity : AppCompatActivity() {
+
+    //AlertDialog used to add a new product to the master list
+    lateinit var dialog: AlertDialog
+    lateinit var dialogBuilder: AlertDialog.Builder
+    val dialogItems = arrayOf("Yes", "No")
+    var dialogResult = ""
+    //lateinit var nameEditText: EditText
+    //lateinit var categoryEditText: EditText
+    //lateinit var layout: LinearLayout
+
+    //Binding to retrieve this activity's views
+    private lateinit var binding: ActivityMasterListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMasterListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        dialogBuilder = AlertDialog.Builder(this)
+
+        val btnAddProduct = binding.btnAddProduct
+
+        btnAddProduct.setOnClickListener {
+
+            var layout:LinearLayout = LinearLayout(this)
+            layout.orientation = LinearLayout.VERTICAL
+            layout.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.FILL_PARENT,
+                LinearLayout.LayoutParams.FILL_PARENT
+            )
+
+
+            //layout.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.Fill_PARENT)
+            var nameEditText: EditText = EditText(this)
+            nameEditText.setPadding(5,10,5,20)
+            nameEditText.hint = "Product's name"
+
+            var categoryEditText: EditText = EditText(this)
+            categoryEditText.setPadding(5,10,5,20)
+            categoryEditText.hint = "Product's category"
+
+            layout.addView(nameEditText)
+            layout.addView(categoryEditText)
+
+            //dialogBuilder.setView(nameEditText)
+            //dialogBuilder.setView(categoryEditText)
+            dialogBuilder.setView(layout)
+            dialogBuilder.setTitle("Enter name of the product you wish to add")
+
+            dialogBuilder.setPositiveButton("Create", DialogInterface.OnClickListener { dialog, whichButton ->
+                //What ever you want to do with the value
+                //val editTextVal: Editable = editText.text.toString()
+                //OR
+                val productName: String = nameEditText.text.toString()
+                val productCategory: String = categoryEditText.text.toString()
+                val newProduct: Product = Product(productName, Category(productCategory))
+
+                var frag = MasterListCellFragment.newInstance(newProduct.name, newProduct.category.name)
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.products, frag).commit()
+                dialog.dismiss()
+            })
+
+            dialogBuilder.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, whichButton ->
+                // what ever you want to do with No option.
+                dialog.dismiss()
+            })
+
+             dialogBuilder.show()
+        }
+
+    }
+}
