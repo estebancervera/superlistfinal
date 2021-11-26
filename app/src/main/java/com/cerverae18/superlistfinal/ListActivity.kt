b6441dd5f22f7dd8617911ba98aa258737d126be
Andoni <andoni_rdgz@outlook.com>
2021-11-26 00:17:48 -0600

    Added ListActivity documentation

--- app/src/main/java/com/cerverae18/superlistfinal/ListActivity.kt
+++ app/src/main/java/com/cerverae18/superlistfinal/ListActivity.kt
@@ -1,30 +1,42 @@
 package com.cerverae18.superlistfinal
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.widget.Switch
-import android.widget.TextView
 import androidx.activity.viewModels
 import com.cerverae18.superlistfinal.databinding.ActivityListBinding
 import com.cerverae18.superlistfinal.fragments.ListProductCellFragment
 import com.cerverae18.superlistfinal.logic.*
-import com.cerverae18.superlistfinal.logic.entities.relations.ListWithProducts
+import com.cerverae18.superlistfinal.logic.entities.relations.ProductFromList
 
+/**
+ * An Activity to display the date list
+ *
+ * This class creates an Activity for the User to view a particular date list
+ *  and selecting which products has been taken so far
+ *
+ * @property binding the reference to the UI components in the layout associated to the Activity.
+ * @property products the list of the products from a list
+ * @property sortingSwitch two-state toggle switch widget that can select between two options.
+ * @property listViewModel ViewModel that allows async operations to the database involving List
+ * @property productListViewModel ViewModel that allows async operations to the database involving ProductListCrossRef
 
+ */
+@SuppressLint("UseSwitchCompatOrMaterialCode")
 class ListActivity : AppCompatActivity() {
 
-    // Binding to retrieve this activity's views
     private lateinit var binding: ActivityListBinding
-    private lateinit var products: List<ListWithProducts>
+    private lateinit var products: List<ProductFromList>
     private lateinit var sortingSwitch: Switch
 
-    val productListViewModel: ProductListViewModel by viewModels {
+    /* View Models */
+    internal val productListViewModel: ProductListViewModel by viewModels {
         ProductListViewModelFactory((application as GeneralApplication).productListRepository)
     }
 
-    val listViewModel: ListViewModel by viewModels {
+    private val listViewModel: ListViewModel by viewModels {
         ListViewModelFactory((application as GeneralApplication).listRepository)
     }
 
@@ -33,35 +45,42 @@ class ListActivity : AppCompatActivity() {
         binding = ActivityListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Sets up the back arrow for the user and runs the code on onSupportNavigateUp()
         this.supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        //this.supportActionBar?.setDisplayShowTitleEnabled(false)
 
-
-
+        // Get the selected list id from the extras passed in the intent
         val listId = intent.getStringExtra(EXTRA.EXTRA_LIST_ID)
 
-
-        Log.i("EACS", "$listId")
         if (listId != null) {
+            // If the id is not null get the selected list asynchronously from the database and observes for changes
             listViewModel.getListById(listId).observe(this, { list ->
                 this.supportActionBar?.title = list.name
             })
 
+            // Populate the products list with the database data
             productListViewModel.productsFromList(listId).observe(this, { products ->
                 this.products = products
+                // Set the activity's fragments by alphabetical order
                 setFragmentsByAlphabeticalOrder()
-
             })
         }
 
 
     }
 
+    /**
+     * This method is called whenever the user chooses to navigate Up within your application's activity hierarchy from the action bar.
+     * @return True if the activity completed successfully and ended and false if not.
+     */
     override fun onSupportNavigateUp(): Boolean {
         finish()
         return true
     }
 
+    /**
+     * This method add a number of [ListProductCellFragment] to the activity sorted by checked status first
+     * and then by alphabetical ascending order (A to Z).
+     */
     private fun setFragmentsByAlphabeticalOrder() {
         for (product in products.sortedWith(compareBy({it.checked}, {it.name}))) {
             val frag = ListProductCellFragment.newInstance(product)
@@ -69,6 +88,11 @@ class ListActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * This method add a number of [ListProductCellFragment] to the activity sorted by checked status first
+     * then by category name in ascending order (A to Z) and finally by product name in each category
+     * in alphabetical ascending order to (A to Z).
+     */
     private fun setFragmentsByCategory() {
         for (product in products.sortedWith(compareBy({it.checked},{it.category}, {it.name})) ){
             val frag = ListProductCellFragment.newInstance(product)
@@ -76,34 +100,41 @@ class ListActivity : AppCompatActivity() {
         }
     }
 
-     fun removeAllFragments() {
+    /**
+     * This method remove all the fragments in the activity.
+     */
+    fun removeAllFragments() {
         for (fragment in supportFragmentManager.fragments) {
             supportFragmentManager.beginTransaction().remove(fragment).commit()
         }
     }
 
+
+    /**
+     * Initialize the contents of the Activity's standard options menu.
+     */
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_list, menu)
-        val itemswitch = menu!!.findItem(R.id.sorting_switch)
-        itemswitch.setActionView(R.layout.switch_item)
 
-        sortingSwitch = menu.findItem(R.id.sorting_switch).actionView.findViewById(R.id.layout_switch) as Switch
+        sortingSwitch = menu?.findItem(R.id.sorting_switch)?.actionView?.findViewById(R.id.layout_switch) as Switch
 
-        sortingSwitch.setOnCheckedChangeListener { _, state ->
+        sortingSwitch.setOnCheckedChangeListener { _, _ ->
           updateOnChecked()
         }
         return super.onCreateOptionsMenu(menu)
     }
 
+    /**
+     * This method remove and then add a number of [ListProductCellFragment] sorted depending
+     * on the [sortingSwitch] state.
+     */
     private fun updateOnChecked(){
-
         removeAllFragments()
-        if ( sortingSwitch.isChecked) {
+        if (sortingSwitch.isChecked) {
             setFragmentsByCategory()
         } else {
             setFragmentsByAlphabeticalOrder()
         }
     }
-
 
 }
